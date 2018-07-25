55da6c9934d4cf0564fff893d033c0f33ef14d16
falcao998 <felipesouza_analista@yahoo.com.br>
2018-07-24 23:12:33 -0300

    CADASTRO_USUARIO

--- src/com/sys/automacao/comercial/view/UsuarioViewController.java
+++ src/com/sys/automacao/comercial/view/UsuarioViewController.java
@@ -1,5 +1,8 @@
 package com.sys.automacao.comercial.view;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import com.sys.automacao.comercial.util.ExchangeStage;
 
 import javafx.fxml.FXML;
@@ -11,6 +14,30 @@ public class UsuarioViewController {
 	
 	@FXML
 	AnchorPane anchorPane;
+	
+	@FXML
+	JFXTextField tfMatricula;
+	
+	@FXML
+	JFXTextField tfNome;
+	
+	@FXML
+	JFXTextField tfSenha;
+	
+	@FXML
+	JFXTextField tfEmail;
+	
+	@FXML
+	JFXComboBox cbNivel;
+	
+	@FXML
+	JFXComboBox cbStatus;
+	
+	@FXML
+	JFXButton butSair;
+	
+	@FXML
+	JFXButton butSalvar;
 	
 	@FXML
 	public void handleSair() {

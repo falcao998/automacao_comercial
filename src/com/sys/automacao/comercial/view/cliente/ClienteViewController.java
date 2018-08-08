b4633145156bb94951b060d7622738be9396f130
Felipe Souza <felipesouza_analista@yahoo.com.br>
2018-08-08 18:56:57 -0300

    Cliente_Tela

--- src/com/sys/automacao/comercial/view/cliente/ClienteViewController.java
+++ src/com/sys/automacao/comercial/view/cliente/ClienteViewController.java
@@ -1,9 +1,14 @@
 package com.sys.automacao.comercial.view.cliente;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
-import com.sys.automacao.comercial.dao.service.UsuarioDaoService;
+import com.sys.automacao.comercial.dao.service.ClienteDaoService;
 import com.sys.automacao.comercial.model.Cliente;
 import com.sys.automacao.comercial.model.NivelUsuarioEnum;
 import com.sys.automacao.comercial.model.StatusUsuarioEnum;
@@ -19,25 +24,94 @@ public class ClienteViewController {
 	
 	ExchangeStage exchangeStage = new ExchangeStage();
 	
-	UsuarioDaoService service = new UsuarioDaoService();
+	ClienteDaoService service = new ClienteDaoService();
 	
 	@FXML
 	AnchorPane anchorPane;
 	
 	@FXML
-	JFXTextField tfMatricula;
+	JFXTextField tfCodigo;
 	
 	@FXML
 	JFXTextField tfNome;
 	
+	/*@FXML
+	JFXDatePicker dataCadastro;*/
+	
 	@FXML
-	JFXTextField tfSenha;
+	JFXTextField telefone1;
+	
+	@FXML
+	JFXTextField telefone2;
+	
+	@FXML
+	JFXTextField telefone3;
 	
 	@FXML
 	JFXTextField tfEmail;
 	
 	@FXML
-	JFXComboBox<NivelUsuarioEnum> cbNivel;
+	JFXTextField site;
+	
+	/*@FXML
+	JFXTextField limiteCredito;*/
+	
+	@FXML
+	JFXTextArea observacao;
+	
+	@FXML
+	JFXTextField vendedorPreferencial;
+	
+	@FXML
+	JFXTextField codigoRegimeTributario;
+	
+	@FXML
+	JFXTextField ibge;
+	
+	/*@FXML
+	JFXTextField fotoCLiente;*/
+	
+	/*@FXML
+	JFXTextField dataNascimento;*/
+	
+	@FXML
+	JFXTextField cep;
+	
+	@FXML
+	JFXTextField endereco;
+	
+	@FXML
+	JFXTextField numero;
+	
+	@FXML
+	JFXTextField complemento;
+	
+	@FXML
+	JFXTextField bairro;
+	
+	@FXML
+	JFXTextField cidade;
+	
+	@FXML
+	JFXTextField estado;
+	
+	/*@FXML
+	JFXTextField pessoaFIsica;*/
+	
+	@FXML
+	JFXTextField cpf;
+	
+	@FXML
+	JFXTextField identidade;
+	
+	@FXML
+	JFXTextField cnpj;
+	
+	@FXML
+	JFXTextField inscricaoEstaual;
+	
+	@FXML
+	JFXTextField tfSenha;
 	
 	@FXML
 	JFXComboBox<StatusUsuarioEnum> cbStatus;
@@ -60,11 +134,10 @@ public class ClienteViewController {
 		cbStatus.setItems( FXCollections.observableArrayList( StatusUsuarioEnum.values()));
 		
 		if(cliente != null) {
-			tfMatricula.setText(cliente.getCodigo());
+			tfCodigo.setText(cliente.getCodigo());
 			tfNome.setText(cliente.getNome());
 			tfSenha.setText(cliente.getCep());
 			tfEmail.setText(cliente.getEmail());
-			cbNivel.setValue(NivelUsuarioEnum.valueOf(cliente.getBairro()));
 			cbStatus.setValue(StatusUsuarioEnum.valueOf(cliente.getStatus()));
 			butApagar.setDisable(false);
 			
@@ -84,8 +157,7 @@ public class ClienteViewController {
 	
 	@FXML
 	public void handleSalvar() {
-		service.save((cliente != null ? cliente.getId() : null), tfMatricula.getText(), tfNome.getText(), tfSenha.getText(), tfEmail.getText(),
-					 cbStatus.getValue().toString(), cbNivel.getValue().toString());
+//		service.save((cliente != null ? cliente.getId() : null), tfCodigo.getText(), tfNome.getText(), tfSenha.getText(), tfEmail.getText(),cbStatus.getValue().toString(), cbNivel.getValue().toString());
 		exchangeStage.closePanelIntern(anchorPane);
 	}
 }

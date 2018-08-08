490319f56e73c3a9b8e07efed594b357405f23f3
Felipe Souza <felipesouza_analista@yahoo.com.br>
2018-08-08 18:34:26 -0300

    Cliente_Tela

--- src/com/sys/automacao/comercial/view/cliente/ClienteViewController.java
+++ src/com/sys/automacao/comercial/view/cliente/ClienteViewController.java
@@ -1,12 +1,91 @@
 package com.sys.automacao.comercial.view.cliente;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import com.sys.automacao.comercial.dao.service.UsuarioDaoService;
 import com.sys.automacao.comercial.model.Cliente;
+import com.sys.automacao.comercial.model.NivelUsuarioEnum;
+import com.sys.automacao.comercial.model.StatusUsuarioEnum;
+import com.sys.automacao.comercial.util.ExchangeStage;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class ClienteViewController {
 	
 	private Cliente cliente;
-
+	
+	ExchangeStage exchangeStage = new ExchangeStage();
+	
+	UsuarioDaoService service = new UsuarioDaoService();
+	
+	@FXML
+	AnchorPane anchorPane;
+	
+	@FXML
+	JFXTextField tfMatricula;
+	
+	@FXML
+	JFXTextField tfNome;
+	
+	@FXML
+	JFXTextField tfSenha;
+	
+	@FXML
+	JFXTextField tfEmail;
+	
+	@FXML
+	JFXComboBox<NivelUsuarioEnum> cbNivel;
+	
+	@FXML
+	JFXComboBox<StatusUsuarioEnum> cbStatus;
+	
+	@FXML
+	JFXButton butSair;
+	
+	@FXML
+	JFXButton butSalvar;
+	
+	@FXML
+	JFXButton butApagar;
+	
 	public ClienteViewController(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	@FXML
+	public void initialize() {
+		cbStatus.setItems( FXCollections.observableArrayList( StatusUsuarioEnum.values()));
+		
+		if(cliente != null) {
+			tfMatricula.setText(cliente.getCodigo());
+			tfNome.setText(cliente.getNome());
+			tfSenha.setText(cliente.getCep());
+			tfEmail.setText(cliente.getEmail());
+			cbNivel.setValue(NivelUsuarioEnum.valueOf(cliente.getBairro()));
+			cbStatus.setValue(StatusUsuarioEnum.valueOf(cliente.getStatus()));
+			butApagar.setDisable(false);
+			
+		}
+	}
+	
+	@FXML
+	public void handleDeletar() {
+		service.remove(cliente.getId());
+		exchangeStage.closePanelIntern(anchorPane);
+	}
+	
+	@FXML
+	public void handleSair() {
+		exchangeStage.closePanelIntern(anchorPane);
+	}
+	
+	@FXML
+	public void handleSalvar() {
+		service.save((cliente != null ? cliente.getId() : null), tfMatricula.getText(), tfNome.getText(), tfSenha.getText(), tfEmail.getText(),
+					 cbStatus.getValue().toString(), cbNivel.getValue().toString());
+		exchangeStage.closePanelIntern(anchorPane);
 	}
 }
